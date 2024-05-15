8bc2ba84d11172de0d73d4fe60af07d4992c278d
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-15 15:15:44 +0200

    complete MetGala

---
+++ src/org/learning/MetGala.java
@@ -0,0 +1,29 @@
+package org.learning;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MetGala {
+    public static void main(String[] args) {
+        String[] persons = {"Jennifer Lopez", "Zendaya", "Chris Hemsworth", "Bad Bunny", "Cara Delevingne", "Kendall Jenner", "Uma Thurman", "Damiano David", "Luca Guadagnino"};
+        System.out.println(Arrays.toString(persons));
+        String userName;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Come ti chiami?");
+        userName = scanner.nextLine();
+        System.out.println(userName);
+        boolean flag = false;
+        for (String person : persons) {
+            if (userName.equals(person)) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag){
+            System.out.println("Puoi entrare! Buon divertimento :D");
+        } else {
+            System.out.println("Accesso Negato! Puoi tornare da dove sei venuto.");
+        }
+    }
+}
